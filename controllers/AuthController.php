92e03cfc9155ecdf93efa37bbbdb1000c03153cd
Yassin Khaled Abd ElSamie Ali <yassinkhaled460@yahoo.com>
2025-05-16 21:44:05 +0300

    Add files via upload

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -5,6 +5,9 @@
 
 require_once '../core/BaseController.php';
 require_once '../models/User.php';
+require_once '../models/LoginContext.php';
+require_once '../config/Database.php';
+require_once '../models/LoginStrategies/LoginStrategy.php';
 require_once '../models/LoginStrategies/SocialLogin.php';
 require_once '../models/LoginStrategies/EmailLogin.php';
 require_once 'JsonController.php';
@@ -18,13 +21,22 @@ class AuthController extends BaseController {
         parent::__construct();
         $this->userModel = new User(Database::getInstance()->getConnection());
         $this->loginContext = new LoginContext();
-        session_start();
+        // session_start();
     }
 
     public function signup($data) {
+
+        if ($data['type'] === 'beneficiary') {
+            $data['email'] = null;
+            $data['password'] = '';
+            $data['rePassword'] = ''; // Match empty passwords
+            $data['login_type'] = '';
+            $data['skills'] = '';
+        }
+
         $this->userModel->__constructUser($data);
 
-        if($data['password']==$data['rePassword']){
+        if($data['type'] === 'beneficiary' || $data['password']==$data['rePassword']){
         // Call the User model's register method
         $response=$this->userModel->register($this->userModel);
 
@@ -58,6 +70,7 @@ class AuthController extends BaseController {
             $_SESSION['user_name'] = $this->userModel->getName();
             $_SESSION['user_email'] = $this->userModel->getEmail();
             $_SESSION['user_type'] = $this->userModel->getType();
+            $_SESSION['user_role'] = $this->userModel->getRole();
             $response['statusCode']=200;
             return jsonResponse($response);
         } else{
